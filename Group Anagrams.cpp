9cf7f3269e6c524063adec31ba1416d8d68fb8f4
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-10-10 14:42:37 +0530

    Create Group Anagrams.cpp

---
+++ Group Anagrams.cpp
@@ -0,0 +1,36 @@
+// # Intuition
+// <!-- Describe your first thoughts on how to solve this problem. -->
+// My first thought is to store the same letter words in a Data Structure
+// # Approach
+// <!-- Describe your approach to solving the problem. -->
+// -> Declare the map which stores the string and a vector of strings
+// -> Iterate through the array
+// -> sort the element letter wise. for eg. eat - aet
+// -> Now store all the elements which consists of letters 'aet' in a vector of strings declared in map
+// -> iterate through map and store the arrays in a 2D array.
+
+// # Complexity
+// - Time complexity:
+// O(Nklogk)
+
+// - Space complexity:
+// O(Nk)
+
+// # Code
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groupanagrams;
+        for(const string s: strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            groupanagrams[key].push_back(s);
+        }
+        vector<vector<string>> group;
+        for(auto const &pair: groupanagrams){
+            group.push_back(pair.second);
+        }
+        return group;
+    }
+};
